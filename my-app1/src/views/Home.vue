e50de1fe52d6fa80e6d71bfe5c172b70bbcc4826
if0balino <balino.ical@gmail.com>
2021-08-23 04:22:54 +0700

    basic vue

--- my-app1/src/views/Home.vue
+++ my-app1/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <hello-world />
+  <div class="d-sm-flex indigo">
+    <v-container class="">
+      <h1>ICAL FERIN BALINO</h1>
+    </v-container>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+// import HelloWorld from "../components/HelloWorld";
 
 export default {
-  name: "Home",
+  // name: "Home",
 
-  components: {
-    HelloWorld,
-  },
+  // components: {
+  //   HelloWorld,
+  // },
 };
 </script>
