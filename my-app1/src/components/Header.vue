8c4361b743c96d8d1153232ff43093427085423c
if0balino <balino.ical@gmail.com>
2021-08-27 09:55:44 +0700

    v-app-bar, v-tabs, v-container, v-image

--- my-app1/src/components/Header.vue
+++ my-app1/src/components/Header.vue
@@ -1,8 +1,18 @@
 <template>
-    <div class="primary" >
-        <h2>Ical Ferin Balino <br>
-            <strong class="warning--text">Student At Faculty of Information Technology, Informatics Engineering</strong>
-        </h2>
+    <div >
+        <v-container fluid style="height:670px; padding:0; margin:0;">
+            <v-app-bar src="https://picsum.photos/1920/1080?random" class="d-flex justify-center align-center fill-height" style="height:670px;"> 
+            
+                <template v-slot:img="{ props }">
+                    <v-img v-bind="props" style="height:670px;"></v-img>
+                </template>
+
+                <h2>Ical Ferin Balino <br>
+                    <strong class="warning--text">Student At Faculty of Information Technology, Informatics Engineering</strong>
+                </h2>
+           
+            </v-app-bar>
+        </v-container>
     </div>
 </template>
 
@@ -18,7 +28,6 @@ export default {
         margin: 0;
         font-family: 'Raleway', sans-serif;
         text-align: center;
-        text-transform: initial;
     }
 
     h2 > strong {
--- my-app1/src/components/Navbar.vue
+++ my-app1/src/components/Navbar.vue
@@ -1,9 +1,19 @@
 <template>
-    <nav>
+    <!-- <nav>
         <ul class="white">
             <li v-for="list in lists" :key="list"><a href="" class="warning--text">{{ list }}</a></li>
         </ul>
-    </nav>
+    </nav> -->
+    <v-container fluid style="padding:0; margin:0;">
+        <v-app-bar app light class="white" style="position: sticky;" dense height="15">
+            <template v-slot:extension>
+                <v-tabs  v-model="tab" centered slider-color="warning"> 
+                    <v-tab v-for="list in lists" :key='list'>{{list}}</v-tab>
+                </v-tabs>
+            </template>
+        </v-app-bar>
+        <v-container fluid style="height: 1000px;"></v-container>
+    </v-container>
 </template>
 
 <script>
@@ -34,8 +44,8 @@ export default {
         text-decoration: none;
         display: inline-block;
         list-style-type: none;
-        padding: 10px;
+        /* padding: 10px; */
         text-align: center;
-        margin-right: 20px;
+        /* margin-right: 20px; */
     }
 </style>
